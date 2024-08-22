217806bee26cfb5d36de69d8eed3c5a84cdfa68e
mrfaridnaz <mrfad8392@gmail.com>
2024-08-22 21:58:49 +0530

    The code for return

--- func3.py
+++ func3.py
@@ -1,5 +1,16 @@
+import logging
+
+logging.basicConfig(
+    filename="test1.log",
+    level=logging.INFO,
+    format='%(levelname)s %(asctime)s %(name)s %(message)s')
+
 def test():
-    print("this is my first function")
+    logging.info("this is my first function")
 
-test()
+# return
+def test1():
+    logging.info("this is my function with return no print")
 
+def test2():
+    logging.info("this is function with return")
